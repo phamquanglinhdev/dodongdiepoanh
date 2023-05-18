e503d1e6505d2a7e46c1f4e29ba8c3cd63589683
phamquanglinhhdev <phamquanglinhdev@gmail.com>
2023-05-19 01:28:56 +0700

    edit news manager

---
+++ app/Models/Scopes/NewsScope.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class NewsScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     */
+    public function apply(Builder $builder, Model $model): void
+    {
+        if (backpack_user()->roles == "author") {
+            $builder->where("author_id", backpack_user()->id);
+        }
+    }
+}
