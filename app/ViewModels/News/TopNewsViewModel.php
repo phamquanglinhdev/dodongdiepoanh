96aa949032edb34c999f38a543714b646daa571b
phamquanglinhdev <phamquanglinhdev@gmail.com>
2023-05-19 21:37:47 +0700

    change top news

--- app/ViewModels/News/TopNewsViewModel.php
+++ app/ViewModels/News/TopNewsViewModel.php
@@ -22,7 +22,7 @@ class TopNewsViewModel
     {
         return $this->trendingNews->map(fn(News $news) => new TopNewsObject(
             title: $news["title"],
-            slug: $news["slug"],
+            slug: $news["type_id"] != 3 ? route("new", $news["slug"]) : url($news["body"]),
             type: $news["type_id"] != 3 ? "Tin tức" : "Báo chí",
             updated_at: $news['updated_at'],
             thumbnail: $news["thumbnail"],
@@ -37,7 +37,7 @@ class TopNewsViewModel
     {
         return $this->recentlyNews->map(fn(News $news) => new TopNewsObject(
             title: $news["title"],
-            slug: $news["slug"],
+            slug: $news["type_id"] != 3 ? route("new", $news["slug"]) : url($news["body"]),
             type: $news["type_id"] != 3 ? "Tin tức" : "Bao chí",
             updated_at: $news["updated_at"],
             thumbnail: $news["thumbnail"],
