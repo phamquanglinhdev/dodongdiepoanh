5b63959620a2ce6a7d252bc48f3056ceb91a019b
phamquanglinhdev <phamquanglinhdev@gmail.com>
2023-05-19 21:19:35 +0700

    change top news

--- app/ViewModels/News/TopNewHomeViewModel.php
+++ app/ViewModels/News/TopNewHomeViewModel.php
@@ -22,7 +22,7 @@ class TopNewHomeViewModel
     {
         return $this->businessNews->map(fn(News $news) => new TopNewsObject(
             title: $news["title"],
-            slug: route('new', $news["slug"]),
+            slug: url($news["body"]),
             type: "Tin tức thường",
             updated_at: $news["updated_at"],
             thumbnail: $news["thumbnail"],
@@ -37,7 +37,7 @@ class TopNewHomeViewModel
     {
         return $this->normalNews->map(fn(News $news) => new TopNewsObject(
             title: $news["title"],
-            slug: url($news["body"]),
+            slug: route('new', $news["slug"]),
             type: "Báo chí nói gì về chúng tôi",
             updated_at: $news["updated_at"],
             thumbnail: $news["thumbnail"],
