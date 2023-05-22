8c4f576cebbd4c9ac7505cd1c20eda63f5865ce8
phamquanglinhdev <phamquanglinhdev@gmail.com>
2023-05-22 22:33:35 +0700

    change top news

--- resources/views/components/search_bar.blade.php
+++ resources/views/components/search_bar.blade.php
@@ -2,7 +2,7 @@
     <form action="{{route("products.search")}}">
         <div class="input-group mb-3">
             <input type="text" class="form-control" name="params" placeholder="Tìm kiếm ..." aria-label="" aria-describedby="button-addon2">
-            <button class="shadow-0 btn bg-light" type="button" id="button-addon2">
+            <button class="shadow-0 btn bg-light" type="submit" id="button-addon2">
                 <i class="fas fa-search text-main"></i>
             </button>
         </div>
--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -5,7 +5,6 @@
     Đồ đồng Điệp Oanh
 @endsection
 @section("content")
-
     @include("components.carousel")
     <div class="container-fluid">
         <div class="px-lg-5 mt-lg-5">
