332dd4479b85b8806cac3d2db79119a39e4d3bbb
phamquanglinhdev <phamquanglinhdev@gmail.com>
2023-05-22 21:57:18 +0700

    change top news

--- resources/views/components/top-video.blade.php
+++ resources/views/components/top-video.blade.php
@@ -4,7 +4,7 @@
             <span>VIDEO NỔI BẬT</span>
         </div>
     </div>
-    <div class="col-6 m-auto py-3 ">
+    <div class="col-12 m-auto py-3 ">
         <div class="ratio ratio-16x9">
             <a target="_blank" href="https://youtube.com/clip/Ugkxxv3ePuax8bwfaVOr5N5jw-QE1EIm7Sou">
                 <img src="{{asset("img/youtube.png")}}" class="w-100 rounded">
